8dbb11ba08992d25159b5e8d0d090a1c123cebf8
kwesitheDev <174883738+KwesitheDev@users.noreply.github.com>
2025-10-08 13:54:50 +0100

    nativeRouter added to App

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 //import { StyleSheet,  View } from 'react-native';
 import React from 'react';
 import Main from './src/components/Main';
+import { NativeRouter } from 'react-router-native';
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
   return (
-    <Main />
+    <>
+      <NativeRouter>
+        <Main />
+      </NativeRouter>
+      <StatusBar style= "auto"/>
+    </>
+
   );
 }
 
