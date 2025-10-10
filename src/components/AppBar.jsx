932b08508076832e8f7fa017586875bd549180c4
kwesithedev <174883738+KwesitheDev@users.noreply.github.com>
2025-10-10 18:48:34 +0000

    appbar conditionally rendering signin/signout

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,9 +1,17 @@
-import { View, StyleSheet, Text, /*Pressable*/ ScrollView } from 'react-native';
+import { View, StyleSheet, Text, Pressable, ScrollView } from 'react-native';
 import Constants from 'expo-constants';
 import theme from '../../theme';
 import React from 'react';
 import { Link } from 'react-router-native';
+import { GET_ME } from '../graphql/queries';
+import AuthStorage from '../utils/authStorage';
+import { useQuery, useApolloClient } from '@apollo/client';
+import { useNavigate } from 'react-router-native';
+//authStorage
+const authStorage = new AuthStorage();
 
+
+//styles
 const styles = StyleSheet.create({
   container: {
     paddingTop: Constants.statusBarHeight,
@@ -24,15 +32,33 @@ const styles = StyleSheet.create({
 });
 
 const AppBar = () => {
+  const {data} = useQuery(GET_ME); 
+  const apolloClient = useApolloClient();
+  const navigate  = useNavigate();
+
+  const handleSignOut = async () => {
+    await authStorage.removeAccessToken();
+    await apolloClient.resetStore();
+    navigate('/signin');
+  }
+
+  const user = data?.me;
   return (
     <View style={styles.container}>
       <ScrollView horizontal style={styles.scroll} showsHorizontalScrollIndicator={false}>
         <Link to="/" underlayColor="transparent">
           <Text style={styles.tab}>Repositories</Text>
         </Link>
-        <Link to="/signin" underlayColor="transparent">
-          <Text style={styles.tab}>Sign in</Text>
-        </Link>
+
+        {user ? (
+          <Pressable onPress={handleSignOut}>
+            <Text style={styles.tab}>Sign out</Text>
+          </Pressable>
+        ) : (
+          <Link to="/signin" underlayColor="transparent">
+            <Text style={styles.tab}>Sign in</Text>
+          </Link>
+        )}
       </ScrollView>
     </View>
   );
