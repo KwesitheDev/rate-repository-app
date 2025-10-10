25d6c2de1df7e0dfba438b554e7790bf8d92237a
kwesithedev <174883738+KwesitheDev@users.noreply.github.com>
2025-10-10 16:23:37 +0000

    added error to debug error

--- src/hooks/useRepositories.js
+++ src/hooks/useRepositories.js
@@ -1,11 +1,13 @@
 //import { useState, useEffect } from "react";
 import { useQuery } from "@apollo/client";
-import GET_REPOSITORIES from "../graphql/queries";
+import {GET_REPOSITORIES} from "../graphql/queries";
 
 const useRepositories = () => {
-  const { data, loading, refetch } = useQuery(GET_REPOSITORIES, {
+  const { data, loading, refetch, error } = useQuery(GET_REPOSITORIES, {
     fetchPolicy: 'cache-and-network',
   });
+  console.log('GraphQL data:', data);
+  if (error) console.log('GraphQL error:', error);
 
   const repositories = data ? data.repositories : undefined;
 
@@ -13,4 +15,3 @@ const useRepositories = () => {
 };
 
 export default useRepositories;
-
