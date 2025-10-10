4dc99b808e381f79a23197e116322bb7cafb1562
kwesithedev <174883738+KwesitheDev@users.noreply.github.com>
2025-10-10 18:17:36 +0000

    save token, reset client

--- src/hooks/useSignIn.js
+++ src/hooks/useSignIn.js
@@ -1,8 +1,13 @@
-import { useMutation } from '@apollo/client';
+import { useMutation, useApolloClient } from '@apollo/client';
 import { AUTHORIZE } from '../graphql/mutations';
+import AuthStorage from '../utils/authStorage';
+
+
+const authStorage = new AuthStorage();
 
 const useSignIn = () => {
   const [mutate, result] = useMutation(AUTHORIZE);
+  const apolloClient = useApolloClient();
 
   const signIn = async ({ username, password }) => {
     const { data } = await mutate({
@@ -10,8 +15,17 @@ const useSignIn = () => {
         credentials: { username, password },
       },
     });
+
+    const accessToken = data?.authenticate?.accessToken;
+    if (accessToken) {
+      await authStorage.setAccessToken(accessToken);
+    }
+
+    await apolloClient.resetStore();
+
     return data;
   };
+
 
   return [signIn, result];
 };
