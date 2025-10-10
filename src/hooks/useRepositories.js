dff36cb9a30cf95fbe0070574f3e477857dd0451
kwesithedev <174883738+KwesitheDev@users.noreply.github.com>
2025-10-10 06:33:08 +0000

    used getrepo query

--- src/hooks/useRepositories.js
+++ src/hooks/useRepositories.js
@@ -1,24 +1,15 @@
-import { useState, useEffect } from "react";
+//import { useState, useEffect } from "react";
+import { useQuery } from "@apollo/client";
+import GET_REPOSITORIES from "../graphql/queries";
 
 const useRepositories = () => {
-  const [repositories, setRepositories] = useState();
-  const [loading, setLoading] = useState(false);
+  const { data, loading, refetch } = useQuery(GET_REPOSITORIES, {
+    fetchPolicy: 'cache-and-network',
+  });
 
-  const fetchRepositories = async () => {
-    setLoading(true);
+  const repositories = data ? data.repositories : undefined;
 
-    const response = await fetch('http://192.168.167.201:5000/api/repositories');
-    const json = await response.json();
-
-    setLoading(false);
-    setRepositories(json);
-  };
-
-  useEffect(() => {
-    fetchRepositories();
-  }, []);
-
-  return { repositories, loading, refetch: fetchRepositories };
+  return { repositories, loading, refetch };
 };
 
 export default useRepositories;
