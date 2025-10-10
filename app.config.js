f69d681568028e572d0c6ccc99a1e6046da2b103
kwesithedev <174883738+KwesitheDev@users.noreply.github.com>
2025-10-10 16:20:47 +0000

    config exported as a function

--- app.config.js
+++ app.config.js
@@ -1,30 +1,58 @@
-{
-  "expo": {
-    "name": "rate-repository-app",
-    "slug": "rate-repository-app",
-    "version": "1.0.0",
-    "orientation": "portrait",
-    "icon": "./assets/icon.png",
-    "userInterfaceStyle": "light",
-    "splash": {
-      "image": "./assets/splash.png",
-      "resizeMode": "contain",
-      "backgroundColor": "#ffffff"
+import 'dotenv/config';
+
+export default () => ({
+  expo: {
+    name: "rate-repository-app",
+    slug: "rate-repository-app",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
+    splash: {
+      image: "./assets/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff",
     },
-    "assetBundlePatterns": [
-      "**/*"
-    ],
-    "ios": {
-      "supportsTablet": true
+    assetBundlePatterns: ["**/*"],
+    ios: { supportsTablet: true },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#ffffff",
+      },
     },
-    "android": {
-      "adaptiveIcon": {
-        "foregroundImage": "./assets/adaptive-icon.png",
-        "backgroundColor": "#ffffff"
-      }
+    web: { favicon: "./assets/favicon.png" },
+    extra: {
+      apiUri: process.env.APOLLO_URI,
     },
-    "web": {
-      "favicon": "./assets/favicon.png"
-    }
-  }
-}
+  },
+});
+import 'dotenv/config';
+
+export default () => ({
+  expo: {
+    name: "rate-repository-app",
+    slug: "rate-repository-app",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
+    splash: {
+      image: "./assets/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff",
+    },
+    assetBundlePatterns: ["**/*"],
+    ios: { supportsTablet: true },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#ffffff",
+      },
+    },
+    web: { favicon: "./assets/favicon.png" },
+    extra: {
+      apiUri: process.env.APOLLO_URI,
+    },
+  },
+});
