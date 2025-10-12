621824aae4134ac19a4d98b137d39bdf9d6fcdbb
kwesithedev <174883738+KwesitheDev@users.noreply.github.com>
2025-10-12 10:21:13 +0000

    added RepositoryListCOntainer component for testing

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -33,4 +33,28 @@ const RepositoryList = () => {
   );
 };
 
+
+/**
+ * A component that renders a FlatList of RepositoryItems.
+ * The list data is expected to be passed as a prop called 'repositories'.
+ * The data should be in the format of an array of repository node objects.
+ * Each repository node object should have an 'id' property.
+ * The component also assigns a testID of 'repositoryList' to the rendered FlatList.
+ */
+
+export const RepositoryListContainer = ({ repositories }) => {
+  const repositoryNodes = repositories
+    ? repositories.edges.map(edge => edge.node)
+    : [];
+
+  return (
+    <FlatList
+      data={repositoryNodes}
+      renderItem={({ item }) => <RepositoryItem repository={item} />}
+      keyExtractor={item => item.id}
+      testID="repositoryList"
+    />
+  );
+};
+
 export default RepositoryList;
