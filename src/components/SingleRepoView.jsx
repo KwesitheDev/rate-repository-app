e0b7c1812ee540c19e9899ffa4f8a3eb616bf960
kwesithedev <174883738+KwesitheDev@users.noreply.github.com>
2025-10-14 14:14:22 +0000

    imported text, removed fetchpolicy

--- src/components/SingleRepoView.jsx
+++ src/components/SingleRepoView.jsx
@@ -1,35 +1,22 @@
-import React from 'react';
-import { useParams } from 'react-router-native';
 import { useQuery } from '@apollo/client';
-import { FlatList, View, Text } from 'react-native';
-
 import { GET_REPOSITORY } from '../graphql/queries';
+import { useParams } from 'react-router-native';
+import Text from './Text';
 import RepositoryItem from './RepositoryItem';
-import ReviewItem from './ReviewItem';
-
+import React from 'react';
 const SingleRepoView = () => {
   const { id } = useParams();
-
   const { data, loading, error } = useQuery(GET_REPOSITORY, {
-    variables: { repositoryId: id },
-    fetchPolicy: 'cache-and-network',
+    variables: { id },
   });
 
   if (loading) return <Text>Loading...</Text>;
   if (error) return <Text>Error: {error.message}</Text>;
 
   const repository = data.repository;
-  const reviews = repository.reviews.edges.map(edge => edge.node);
 
   return (
-    <FlatList
-      data={reviews}
-      renderItem={({ item }) => <ReviewItem review={item} />}
-      keyExtractor={({ id }) => id}
-      ListHeaderComponent={() => (
-        <RepositoryItem repository={repository} showGithubButton />
-      )}
-    />
+    <RepositoryItem item={repository} showGithubButton />
   );
 };
 
