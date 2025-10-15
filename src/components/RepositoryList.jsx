366347c6927e4cc0e37ead93fb96a05c2ec9098e
kwesithedev <174883738+KwesitheDev@users.noreply.github.com>
2025-10-15 16:34:31 +0000

    logged erros, used refactored components for search and filtering

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -4,6 +4,10 @@ import RepositoryItem from './RepositoryItem';
 import useRepositories from '../hooks/useRepositories';
 import {useNavigate} from 'react-router-native';
 import { Picker } from '@react-native-picker/picker';
+import {useDebounce} from '../hooks/useDebounce';
+import { RepoListContainer } from './RepoListContainer';
+import { useState } from 'react';
+
 
 const styles = StyleSheet.create({
   separator: {
@@ -18,8 +22,13 @@ const styles = StyleSheet.create({
 const ItemSeparator = () => <View style={styles.separator} />;
 
 const RepositoryList = () => {
+  const [searchQuery, setSearchQuery] = useState('');
   const [selectedOrder, setSelectedOrder] = React.useState('latest');
-  const { repositories } = useRepositories({ orderBy, orderDirection });
+  const [debouncedSearchQuery] = useDebounce(searchQuery, 500);
+  
+
+
+  
   const navigate = useNavigate();
 
   let orderBy = 'CREATED_AT';
@@ -33,12 +42,33 @@ const RepositoryList = () => {
     orderDirection = 'ASC';
   }
 
+  const { repositories, loading, error, refetch } = useRepositories({
+    orderBy,
+    orderDirection,
+    searchKeyword: debouncedSearchQuery,
+  });
+
+  if (error) {
+    console.log('GraphQL error:', error);
+  }
+
+  console.log('debouncedSearchQuery:', debouncedSearchQuery);
+  console.log('repositories:', repositories);
+  
+  React.useEffect(() => {
+  refetch({
+    orderBy,
+    orderDirection,
+    searchKeyword: debouncedSearchQuery,
+  });
+}, [debouncedSearchQuery]);
+
   const repositoryNodes = repositories
     ? repositories.edges.map(edge => edge.node)
     : [];
 
 
-    const renderHeader = () => (
+   /* const renderHeader = () => (
     <View style={styles.pickerContainer}>
       <Picker
         selectedValue={selectedOrder}
@@ -49,16 +79,17 @@ const RepositoryList = () => {
         <Picker.Item label="Lowest rated repositories" value="lowest" />
       </Picker>
     </View>
-  );
+  );*/
 
 
   return (
-    <FlatList
-      data={repositoryNodes}
-      ItemSeparatorComponent={() => <View style={styles.separator} />}
-      renderItem={({ item }) => <RepositoryItem item={item} />}
-      keyExtractor={(item) => item.id}
-      ListHeaderComponent={renderHeader}
+    <RepoListContainer
+      repositories={repositories}
+      selectedOrder={selectedOrder}
+      setSelectedOrder={setSelectedOrder}
+      searchQuery={searchQuery}
+      setSearchQuery={setSearchQuery}
+      navigate={navigate}
     />
   );
 };
