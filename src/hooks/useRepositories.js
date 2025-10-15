3f694a24ba5494befa7d008bc3d3cccca3716a83
kwesithedev <174883738+KwesitheDev@users.noreply.github.com>
2025-10-15 09:15:22 +0000

    modified useRepository to include reviews

--- src/hooks/useRepositories.js
+++ src/hooks/useRepositories.js
@@ -3,15 +3,15 @@ import { useQuery } from "@apollo/client";
 import {GET_REPOSITORIES} from "../graphql/queries";
 
 const useRepositories = () => {
-  const { data, loading, refetch, error } = useQuery(GET_REPOSITORIES, {
+  const { data, loading, refetch, error, fetchMore } = useQuery(GET_REPOSITORIES, {
     fetchPolicy: 'cache-and-network',
   });
   console.log('GraphQL data:', data);
   if (error) console.log('GraphQL error:', error);
 
   const repositories = data ? data.repositories : undefined;
-
-  return { repositories, loading, refetch };
+  const reviews = repositories ? repositories.edges.map(edge => edge.node) : [];
+  return { repositories, loading, refetch, fetchMore, reviews };
 };
 
 export default useRepositories;
