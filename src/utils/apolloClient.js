ed3fd257ffed6773ed24533b0831706b25a0b6e4
kwesithedev <174883738+KwesitheDev@users.noreply.github.com>
2025-10-15 17:12:18 +0000

    added auth token

--- src/utils/apolloClient.js
+++ src/utils/apolloClient.js
@@ -1,15 +1,34 @@
-import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
+import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import Constants from 'expo-constants';
+import AuthStorage from './authStorage';
 
-const { apiUri } = Constants.expoConfig.extra; 
+const authStorage = new AuthStorage();
 
 const createApolloClient = () => {
+
+  const apiUri =
+    Constants.expoConfig?.extra?.apiUri ||
+    Constants.manifest?.extra?.apiUri 
+
+  const httpLink = createHttpLink({
+    uri: apiUri,
+  });
+
+  const authLink = setContext(async (_, { headers }) => {
+    const token = await authStorage.getAccessToken();
+    return {
+      headers: {
+        ...headers,
+        authorization: token ? `Bearer ${token}` : '',
+      },
+    };
+  });
+
   return new ApolloClient({
-    link: new HttpLink({ uri: apiUri }), 
+    link: authLink.concat(httpLink),
     cache: new InMemoryCache(),
   });
 };
 
 export default createApolloClient;
-
-
