422047a129d24e0e2d57fb288bada78fa196ac7d
kwesithedev <174883738+KwesitheDev@users.noreply.github.com>
2025-10-16 15:52:37 +0000

    useRepository hook to use fetchMore

--- src/hooks/useRepositories.js
+++ src/hooks/useRepositories.js
@@ -3,16 +3,31 @@ import { useQuery } from "@apollo/client";
 import {GET_REPOSITORIES} from "../graphql/queries";
 
 const useRepositories = (variables) => {
-  const { data, loading, refetch, error, fetchMore } = useQuery(GET_REPOSITORIES, {
+  const { data, loading, refetch, error, fetchMore, ...result} = useQuery(GET_REPOSITORIES, {
     fetchPolicy: 'cache-and-network',
     variables,
   });
   console.log('GraphQL data:', data);
   if (error) console.log('GraphQL error:', error);
 
+  const handleFetchMore = () => {
+    const canFetchMore = !loading && data?.repositories.pageInfo.hasNextPage;
+
+    if (!canFetchMore) {
+      return;
+    }
+
+    fetchMore({
+      variables: {
+        after: data.repositories.pageInfo.endCursor,
+        ...variables,
+      },
+    });
+  };
+
   const repositories = data ? data.repositories : undefined;
   const reviews = repositories ? repositories.edges.map(edge => edge.node) : [];
-  return { repositories, loading, refetch, fetchMore, reviews };
+  return { repositories, loading, refetch, fetchMore: handleFetchMore, reviews, ...result };
 };
 
 export default useRepositories;
